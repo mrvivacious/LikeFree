26ff548e5bb9ad824b43b9f8ff73ce76534f6269
Vivek Bhookya <v.bhookya98@gmail.com>
2018-06-21 13:05:16 -0500

    0.0.5.2 store update - Notification support

--- likefree.js
+++ likefree.js
@@ -1,6 +1,9 @@
 // LikeFree
 // @Author Vivek Bhookya
 // The scripts that remove the likes
+// TODO:
+
+chrome.runtime.setUninstallURL('https://docs.google.com/forms/d/e/1FAIpQLSdzaT3XlrWHJfnzKjUzrNTCFZJBk9HIO0LskHhu4YNH7z_hOQ/viewform?usp=sf_link');
 
 // Run script whenever user scrolls....
 // Instagram's list for their feed is dynamic.
@@ -8,15 +11,8 @@
 //  too far after it. .scroll() and .mousemove() ensures that my script will match
 //  Instagram's loading scripts
 $(document).ready(function () {
-  //Initial remove on pageload
-  $("._nt9ow").remove();
-  $("._sokb7").remove();
-  $("._nlmjy").remove();
-  $("UFIRow UFILikeSentence _4204 _4_dr").remove();
-  $("._hly").remove();
-  $(".UFICommentLikeButton").remove();
-  $(".UFIShareRow").remove();
-  $("._10lp").remove();
+  hide();
+  setInterval(hide, 30);
 
   // Covering all the bases lmao jquery is too OP
   // For when you can't scroll,
@@ -37,35 +33,34 @@ function hide()
     //  by calling the container ids
     // Yes, this assumes that the container IDs are not
     //  going to be the same between IG and FB ;)
-    $(document).ready(function () {
-      // --- INSTAGRAM --- //
-      $("._nt9ow").remove();                            // Photos
-      $("._sokb7").remove();                            // Videos
-      $("._nlmjy").remove();                            // Photo pop-ups on profile
-      $("._gt9ku").remove();                            // The tiny heart notification
+           // --- INSTAGRAM --- //
+           $(".y9v3U").remove();                            // Photos
+           $("._9Ytll").remove();                            // Videos
+           $(".uk0Yc").remove();                             // The tiny heart notification
+           // $("._5ugh").remove();    // Goodbye notifications
 
-      // --- FACEBOOK --- //
-      $("._4_dr").remove();                             // "Liked by"
-      $("UFIRow UFILikeSentence _4204 _4_dr").remove(); // "Like bar" under posts
-      $("._hly").remove();                              // Reaction number in comments
-      $(".UFICommentLikeButton").remove();              // Reaction icon in comments
-      $(".UFIShareRow").remove();                       // "Times shared" row
-      $("._10lp").remove();                             // Like/reactions white bubble under comments
-      // $("._20h5").remove();                             // Video pop-up
-      // $("._36_q").remove();                             // Video pop-up
-      // $(".fcg").remove();
-      $("._1g5v.js_b4").remove();
-      $("._1vaq").remove();
-      $("._1t6k").remove();
+           // --- FACEBOOK --- //
+           $("._4_dr").remove();                             // "Liked by"
+           $("UFIRow UFILikeSentence _4204 _4_dr").remove(); // "Like bar" under posts
+           $("._hly").remove();                              // Reaction number in comments
+           $(".UFICommentLikeButton").remove();              // Reaction icon in comments
+           $(".UFIShareRow").remove();                       // "Times shared" row
+           $("._10lp").remove();                             // Like/reactions white bubble under comments
+           $("._1g5v.js_b4").remove();
+           $("._1vaq").remove();
+           $("._1t6k").remove();
+
+           $("#notificationsCountValue").remove();
+           $("#mercurymessagesCountValue").remove();
+           $("#requestsCountValue").remove();
+
+           // When FACEBOOK posts fill the screen
+           $("._ipn").remove();    // Reaction number (1/2)
+           $("._1g5v").remove();   // Reaction count (2/2)
+           $(".js_1kz").remove();  // "Liked by"
+           $("._50f8").remove();   // View count
+           $("._2x4v").remove();   // Reaction number in comments
+           $(".rfloat _ohf").remove();
 
 
-      // When FACEBOOK posts fill the screen
-      $("._ipn").remove();    // Reaction number (1/2)
-      $("._1g5v").remove();   // Reaction count (2/2)
-      $(".js_1kz").remove();  // "Liked by"
-      $("._50f8").remove();   // View count
-      $("._2x4v").remove();   // Reaction number in comments
-      $(".rfloat _ohf").remove();
-
-    });
 }
